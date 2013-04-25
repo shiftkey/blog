09851deee3cac9853710b59fda93b2f8598f60f5
Brendan Forster <me@brendanforster.com>
2013-04-25 10:51:34 +1000

    added new template and it's resources

---
+++ new/js/config.js
@@ -0,0 +1,14 @@
+/*
+	Halcyonic 3.0 by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+window._5grid_config = {
+	prefix: 'css/style',
+	preset: 'legacy'
+};
+
+window._5grid_ui_config = {
+	preset: 'legacy'
+};
